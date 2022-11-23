9e8408bbe3c238e6850a6d2655296e2efc67247e
Kangas Jyri V <jyri_kangas@outlook.com>
2022-11-23 19:31:09 +0200

    kahden eri tuotteen jälkeen ostoskorin ostokset() palauttaa kaksi ostosta

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -62,3 +62,12 @@ class TestOstoskori(unittest.TestCase):
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.hinta(), 3)
         self.assertEqual(len(self.kori.ostokset()),1)
+    
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorissa_kaksi_ostosta(self):
+        maito = Tuote("Maito", 3)
+        rahka = Tuote("Rahka", 1)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(rahka)
+        
+        self.assertEqual(len(self.kori.ostokset()), 2)
+        
